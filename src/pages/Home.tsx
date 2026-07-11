42884b3252e69fe745a6a9cef81a7bca1755d9dd
TechMessiah <mr.jeromeogwang@gmail.com>
2026-07-11 18:35:07 +0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { QuickActions } from "../components/quick-actions";
+
+export default function Home() {
+  return (
+    <section className="w-full h-screen flex items-center justify-center bg-background">
+      <QuickActions />
+    </section>
+  );
+}
